cb5c8d45b84c4cf0a41bcbed1e397264d095e72e
manoj7gitmanoj.ghadei@gmail.com <manoj.ghadei@gmail.com>
2021-05-25 18:08:35 +0530

    QualFlowVRT Updated with changes to Pkg Name (from VRT to QUalFlowVRT) for all pages & Class

---
+++ src/test/java/com/QualFlowVRT/testcases/testRunner.java
@@ -0,0 +1,36 @@
+/**
+ * @author manoj.ghadei
+ *
+ */
+
+package com.QualFlowVRT.testcases;
+
+import java.io.FileNotFoundException;
+
+import javax.swing.JOptionPane;
+
+import org.testng.TestNG;
+
+import com.QualFlowVRT.Listners.ExtentReporterNG;
+
+public class testRunner {
+	
+	static TestNG testng;
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws FileNotFoundException {		
+		JOptionPane.showMessageDialog(null, "Please ensure the WinAppDriver.exe is launched present "
+				+ "at the location- C:\\Program Files (x86)\\Windows Application Driver", "InfoBox: Alert!", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "For App UID & PW,which Asset & Setup Name to select, "
+				+ "BaseStation IP address, SOP data, Study time in Minutes & Study save comment, "
+				+ "Please update the excel sheet provided with this exe", "InfoBox: Alert!", JOptionPane.INFORMATION_MESSAGE);
+		ExtentReporterNG er = new ExtentReporterNG();
+
+		testng = new TestNG();
+		//testng.setTestClasses(new Class[] {HitNTrialTests.class});
+		testng.setTestClasses(new Class[] {QualificationProcessTest.class});
+
+		testng.run();			
+	}
+
+}
